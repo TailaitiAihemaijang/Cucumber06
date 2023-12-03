7b1e3613631743a7ac396ebf8fc0029c9faf1b4f
Talat <javaucun@gmail.com>
2023-12-03 10:52:22 +0300

    Plugin Eklendi

--- src/test/java/Runners/_02_TestRunnerBelirliFeaturelar.java
+++ src/test/java/Runners/_02_TestRunnerBelirliFeaturelar.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles/_01_Login.feature",
         "src/test/java/FeatureFiles/_02_Country.feature"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin = {"json:target/cucumber/cucumber.json"}
 )
 
 public class _02_TestRunnerBelirliFeaturelar extends AbstractTestNGCucumberTests {
